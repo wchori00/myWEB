c137e00fb193db2738b7bc847243bebd8d28e021
wchori00 <swc198888@gmail.com>
2022-07-01 16:13:29 +0900

    MVC 모델2 실습

---
+++ src/main/java/tommy/mvc/control/ActionFactory.java
@@ -0,0 +1,24 @@
+package tommy.mvc.control;
+
+import tommy.mvc.action.Action;
+import tommy.mvc.action.IndexAction;
+public class ActionFactory {
+	private static ActionFactory factory;
+	private ActionFactory() {}
+	public static synchronized ActionFactory getInstance() {
+		if(factory == null) {
+			factory = new ActionFactory();
+		}
+		return factory;
+	}
+	/*
+	명령어를 받아서 그 명령을 수행할 수 있는 해당 Action 객체를 생성해서 리턴해줌
+	*/
+	public Action getAction(String cmd) {
+		Action action = null;
+		if(cmd.equals("index")) {
+			action = new IndexAction();
+		}
+		return action;
+	}
+}
