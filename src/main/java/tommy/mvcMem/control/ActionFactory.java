15ee8f371ecc34d65f8715b71356a7cbf2505a78
wchori00 <swc198888@gmail.com>
2022-07-01 15:09:29 +0900

    로그인 모델2

---
+++ src/main/java/tommy/mvcMem/control/ActionFactory.java
@@ -0,0 +1,70 @@
+package tommy.mvcMem.control;
+
+import tommy.mvcMem.action.Action;
+//import tommy.mvcMem.action.DeleteFormAction;
+//import tommy.mvcMem.action.DeleteProcAction;
+import tommy.mvcMem.action.IdCheckAction;
+import tommy.mvcMem.action.IndexAction;
+//import tommy.mvcMem.action.LoginFormAction;
+//import tommy.mvcMem.action.LoginProcAction;
+//import tommy.mvcMem.action.LogoutAction;
+//import tommy.mvcMem.action.ModifyFormAction;
+//import tommy.mvcMem.action.ModifyProcAction;
+import tommy.mvcMem.action.RegFormAction;
+
+public class ActionFactory {
+	private static ActionFactory factory;
+	private ActionFactory() {
+	}
+	public static synchronized ActionFactory getInstance() {
+		if(factory == null) {
+			factory = new ActionFactory();
+		}
+		return factory;
+	}
+	public Action getAction(String cmd) {
+		Action action = null;
+		switch(cmd) {
+		case "index":
+			action = new IndexAction();
+			break;
+//		case "login":
+//			action = new LoginFormAction();
+//			break;
+//		case "loginProc":
+//			action = new LoginProcAction();
+//			break;
+//		case "logout":
+//			action = new LogoutAction();
+//			break;
+		case "regForm":
+			action = new RegFormAction();
+			break;
+//		case "regProc":
+//			action = new RegProcAction();
+//			break;
+//		case "modifyForm":
+//			action = new ModifyFormAction();
+//			break;
+//		case "modifyProc":
+//			action = new ModifyProcAction();
+//			break;
+//		case "deleteForm":
+//			action = new DeleteFormAction();
+//			break;
+//		case "deleteProc":
+//			action = new DeleteProcAction();
+//			break;
+		case "idCheck":
+			action = new IdCheckAction();
+			break;
+//		case "zipCheck":
+//			action = new ZipCheckAction();
+//			break;
+			default:
+				action = new IndexAction();
+				break;
+		}
+		return action;
+	}
+}
--- src/main/webapp/life.jsp
+++ src/main/webapp/life.jsp
@@ -8,11 +8,7 @@
 	public void jspDestroy() { // 메소드 재정의
 		System.out.println("jspDestroy() 호출됨");
 	}
-
 %>
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -26,11 +22,9 @@
 	numTwo++;	// 새로고침 할때마다 초기화
 	System.out.println(session.getId());
 %>
-
 <ul>
 <li>Number One: <%=numOne %></li>
 <li>Number Two: <%=numTwo %></li>
 </ul>
-
 </body>
 </html>
