1c5a8e34321976d06119bcded92ca8588337b4a8
wchori00 <swc198888@gmail.com>
2022-07-04 14:31:14 +0900

    게시판 만들기 모델2 -1/2

--- src/main/java/tommy/board/model/BoardDAO.java
+++ src/main/java/tommy/board/model/BoardDAO.java
@@ -1,5 +1,12 @@
 package tommy.board.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardDAO {
 
 	private static BoardDAO instance = null;
@@ -18,8 +25,120 @@ public class BoardDAO {
 	
 	// 이곳에 게시판 작업의 기능 하나하나를 메소드로 추가 할 것이다
 	
+	public int getArticleCount() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int x = 0;
+		try {
+			conn = ConnUtil.getConnection();
+			pstmt = conn.prepareStatement("select count(*) from board");
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				x = rs.getInt(1);
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(rs != null) try {rs.close();} catch(SQLException ex) {}
+			if(pstmt != null) try {pstmt.close();} catch(SQLException ex) {}
+			if(conn != null) try {conn.close();} catch(SQLException ex) {}
+		}
+		return x;
+	}
 	
+	public List<BoardVO> getArticles(int start, int end) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<BoardVO> articleList = null;
+		try {
+			conn = ConnUtil.getConnection();
+			pstmt = conn.prepareStatement("select * from(select rownum rnum, num, writer, email, subject, pass, regdate, readcount, ref, step, depth, content, ip from (select * from board order by ref desc, step asc)) where rnum >= ? and rnum <= ?");
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				articleList = new ArrayList<BoardVO>(5);
+				do {
+					BoardVO article = new BoardVO();
+					article.setNum(rs.getInt("num"));
+					article.setWriter(rs.getString("writer"));
+					article.setEmail(rs.getString("email"));
+					article.setSubject(rs.getString("subject"));
+					article.setPass(rs.getString("pass"));
+					article.setRegdate(rs.getTimestamp("regdate"));
+					article.setReadcount(rs.getInt("readcount"));
+					article.setRef(rs.getInt("ref"));
+					article.setStep(rs.getInt("step"));
+					article.setDepth(rs.getInt("depth"));
+					article.setContent(rs.getString("content"));
+					article.setIp(rs.getString("ip"));
+					articleList.add(article);
+				}while(rs.next());
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(rs != null) try {rs.close();} catch(SQLException ex) {}
+			if(pstmt != null) try {pstmt.close();} catch(SQLException ex) {}
+			if(conn != null) try {conn.close();} catch(SQLException ex) {}
+		}
+		return articleList;
+	}
 	
+	public void insertArticle(BoardVO article) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int num = article.getNum();
+		int ref = article.getRef();
+		int step = article.getStep();
+		int depth = article.getDepth();
+		int number = 0;
+		String sql = "";
+		try {
+			conn = ConnUtil.getConnection();
+			pstmt = conn.prepareStatement("select max(num) from board");
+			rs = pstmt.executeQuery();
+			if(rs.next())
+				number = rs.getInt(1)+1;
+			else
+				number=1;
+			if(num != 0) { // 답변글일 경우
+				sql = "update board set step=step+1 where ref=? and step>?";
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, ref);
+				pstmt.setInt(2, step);
+				pstmt.executeUpdate();
+				step=step+1;
+				depth=depth+1;
+			}else { // 새 글일 경우
+				ref = number;
+				step = 0;
+				depth = 0;
+			} // 쿼리를 작성
+			sql = "insert into board(num, writer, email, subject, pass, regdate, ref, step, depth, content, ip) values(board_seq.nextval,?,?,?,?,?,?,?,?,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, article.getWriter());
+			pstmt.setString(2, article.getEmail());
+			pstmt.setString(3, article.getSubject());
+			pstmt.setString(4, article.getPass());
+			pstmt.setTimestamp(5, article.getRegdate());
+			pstmt.setInt(6, ref);
+			pstmt.setInt(7, step);
+			pstmt.setInt(8, depth);
+			pstmt.setString(9, article.getContent());
+			pstmt.setString(10, article.getIp());
+			pstmt.executeUpdate();
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(rs != null)try {rs.close();}catch(SQLException ex1) {}
+			if(pstmt != null)try {pstmt.close();}catch(SQLException ex2) {}
+			if(conn != null)try {conn.close();}catch(SQLException ex3) {}
+		}
+	}
 	
 	
 }
---
+++ src/main/webapp/board/script.js
@@ -0,0 +1,21 @@
+function writeSave(){
+	if(document.writeform.writer.value=="") {
+		alert("작성자를 입력하십시요");
+		document.writeform.writer.focus();
+		return false;
+	}
+	if(document.writeform.subject.value=="") {
+		alert("제목을 입력하십시요");
+		return false;
+	}
+	if(document.writeform.content.value=="") {
+		alert("내용을 입력하십시요");
+		document.writeform.content.focus();
+		return false;
+	}
+	if(document.writeform.pass.value=="") {
+		alert("비밀번호를 입력하십시요");
+		document.writeform.pass.focus();
+		return false;
+	}
+}
